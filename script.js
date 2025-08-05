f444ff3e125bf1aa97565f5b94f88a34ce1d117e
lou-ship-it <louis.gyde08@gmail.com>
2025-08-05 11:14:49 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function clearPlaceholder(element) {
-    element.placeholder = '';
-  }
-  
-  document.getElementById("reservation-form").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Votre demande de réservation a été enregistrée !");
-  });
