fbc426510ad73bc1c414445ad46bbab796bab637
lou-ship-it <louis.gyde08@gmail.com>
2025-08-05 11:16:49 +0200

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,5 @@
+// Auth logic for login/signup
+function logout() {
+  localStorage.clear();
+  window.location.href = 'login.html';
+}
---
+++ profile.js
@@ -0,0 +1,1 @@
+// Logic for user profile and password change
