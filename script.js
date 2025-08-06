519993a6ada2c0b58033e8a4d1b3544cd3ba6a4e
lou-ship-it <louis.gyde08@gmail.com>
2025-08-07 00:15:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function clearPlaceholder(element) {
+    element.placeholder = '';
+  }
+  
+  document.getElementById("reservation-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Votre demande de réservation a été enregistrée !");
+  });
